d201d95c10306e630b427f24d34559a0efe3a350
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2016-02-02 23:44:35 -0800

    cleanup

--- modules/hello_world/template/block--hello-world.tpl.php
+++ modules/hello_world/template/block--hello-world.tpl.php
@@ -46,11 +46,11 @@
 ?>
 <div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
 
-  <?php print render($title_prefix); ?>
-<?php if ($block->subject): ?>
+    <?php print render($title_prefix); ?>
+<?php if ($block->subject) : ?>
   <h2 class="hello_title" <?php print $title_attributes; ?>><?php print $block->subject ?></h2>
 <?php endif;?>
-  <?php print render($title_suffix); ?>
+    <?php print render($title_suffix); ?>
 
   <div class="content hw-list"<?php print $content_attributes; ?>>
     <?php print $content ?>
